4b5a3f341558f91387666697e21c80d5de423769
Dhruv00421 <120349722+Dhruv00421@users.noreply.github.com>
2024-02-25 13:07:49 +0000

    datatype

---
+++ basics/datatypes.js
@@ -0,0 +1,35 @@
+"Use strict"; //use newer version of js
+
+// alert(3+3) available in browser
+//null -> the variable is empty
+
+let name_1 = d
+
+console.log(name_1);
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => empty value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
