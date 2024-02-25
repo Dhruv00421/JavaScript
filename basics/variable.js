75fe1ad8f7361bf9244799e845538ee70e7d4d08
Dhruv00421 <120349722+Dhruv00421@users.noreply.github.com>
2024-02-25 12:37:46 +0000

    New files

---
+++ basics/variable.js
@@ -0,0 +1,15 @@
+const accountId = 123
+let accountEmail = "dhruv@gmail.com"
+var accountPass = "331122"
+accountCity = "jaipur"
+let accountState    
+
+// accountId = 567 not allowed
+
+accountEmail = "dh@gmail.com"
+accountPass = "5522"
+accountCity = "ahmedabad"
+
+
+
+console.table([accountId, accountEmail, accountPass, accountCity, accountState])
