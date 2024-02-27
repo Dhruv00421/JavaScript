f1e325334c8a19cdc242c22c1b84e7cf864ccae9
Dhruv00421 <120349722+Dhruv00421@users.noreply.github.com>
2024-02-27 18:01:59 +0000

    converrsion

---
+++ basics/conversionoperation.js
@@ -0,0 +1,13 @@
+let score = null
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let isLoggedin = 76
+
+let booleanIsLogged = Boolean(isLoggedin)
+console.log(booleanIsLogged);
